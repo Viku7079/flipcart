07c425beae4a0dfca2fdbbe81a113300bec24e82
RAUSHAN KUMAR <52875540+sitarausha@users.noreply.github.com>
2023-02-26 21:59:11 +0530

    login

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if($_POST['submit']){
         // output data of each row
         while($row = $result->fetch_assoc()) {
           if( $row["email"]==$email &&  $row["password"]==$password){
-            header('Location:');
+            header('Location:index.html');
           }
            
         }
@@ -18,13 +18,7 @@ if($_POST['submit']){
         echo "0 results";
     }
       
-    if ($conn->query($sql) === TRUE) {
-        header('Location:');
-    }
-    else{
-        echo "Something Error, Please try again.";
-    }
-
+    
     $conn->close();
 
     }
