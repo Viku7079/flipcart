64a3d974adb77e57b7123018bbab23efacb9ad36
RAUSHAN KUMAR <52875540+sitarausha@users.noreply.github.com>
2023-02-26 13:33:10 +0530

    login page

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+if($_POST['submit']){
+    require "dbconnection.php";
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $sql="select * from user";
+    $result=$conn->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          if( $row["email"]==$email &&  $row["password"]==$password){
+            header('Location:');
+          }
+           
+        }
+        echo "Incorect Credential Please check Email or Password";
+    } else {
+        echo "0 results";
+    }
+      
+    if ($conn->query($sql) === TRUE) {
+        header('Location:');
+    }
+    else{
+        echo "Something Error, Please try again.";
+    }
+
+    $conn->close();
+
+}
+
+?>
