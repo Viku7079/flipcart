88e1f2d3a0016266229548527905be10de0babec
RAUSHAN KUMAR <52875540+sitarausha@users.noreply.github.com>
2023-02-26 12:42:29 +0530

    change

--- register.php
+++ register.php
@@ -3,16 +3,18 @@
         // Database connection
         require "dbconnection.php";
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $sql="insert into user(name,email,password) values('$name','$email','$password')";
-    if ($conn->query($sql) === TRUE) {
-        header('Location: login.html');
-      }
-      else{
-        echo "Something Error, Please try again."
-      }
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+         
+        //Insert data into table
+        $sql="insert into user(name,email,password) values('$name','$email','$password')";
+        if ($conn->query($sql) === TRUE) {
+            header('Location: login.html');
+        }
+        else{
+            echo "Something Error, Please try again.";
+        }
 
         $conn->close();
     }
